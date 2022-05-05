392abb544c68af22ee55e3cca0f3d08e91738a2b
Heather S. Deter <37884830+hdeter@users.noreply.github.com>
2022-05-05 16:27:13 -0500

    Update file via upload

--- setup.py
+++ setup.py
@@ -11,19 +11,19 @@ Created on Fri Apr  8 14:35:47 2022
 
 from setuptools import setup, find_packages
 
-VERSION = '0.0.9' 
+VERSION = '0.1.0' 
 DESCRIPTION = 'Train Image Segmentation'
 LONG_DESCRIPTION = 'Functions written to streamline generating image feature sets and training sci-kit learn classifiers'
 
 # Setting up
 setup(
-       # the name must match the folder name 'verysimplemodule'
         name="trainsegmentation", 
         version=VERSION,
         author="Heather S. Deter",
         author_email="<hdeter2013@gmail.com>",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
+        url = 'https://github.com/hdeter/trainsegmentation',
         packages=find_packages(),
         install_requires=['numpy','scipy','scikit-image','scikit-learn'], 
         keywords=['python', 'sci-kit learn','image segmentation','image classification'],
